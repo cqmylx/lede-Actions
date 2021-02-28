37623f6037f55ad4a1e022b87679e09d86d50326
cqmylx <67597378+cqmylx@users.noreply.github.com>
2021-02-28 10:54:22 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,3 +20,4 @@ git clone https://github.com/rufengsuixing/luci-app-adguardhome ./package/lean/l
 #wget https://github.com/vernesong/OpenClash/archive/master.zip  #下载OpenClash
 #unzip master.zip  #解压
 #cp -r OpenClash-master/luci-app-openclash package  #复制OpenClash软件包到OpenWrt
+git clone https://github.com/garypang13/luci-app-dnsfilter.git ./package/lean/luci-app-dnsfilter
