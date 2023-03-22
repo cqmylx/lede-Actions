7518c3cd12e867d81d30e46b62bcd33dbb3611c9
cqmylx <67597378+cqmylx@users.noreply.github.com>
2023-03-22 10:36:56 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -23,6 +23,12 @@ sed -i 's/KERNEL_TESTING_PATCHVER:=5.10/KERNEL_TESTING_PATCHVER:=6.1/g' ./target
 #git clone https://github.com/rufengsuixing/luci-app-adguardhome ./package/lean/luci-app-adguardhome
 git clone https://github.com/ujincn/smartdns.git ./package/lean/smartdns
 git clone https://github.com/ujincn/luci-app-smartdns-compat.git ./package/lean/luci-app-smartdns
+#git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
+git clone https://github.com/fw876/helloworld.git package/ssr
+git clone https://github.com/firker/diy-ziyong.git package/diy-ziyong
+git clone https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall
+git clone -b luci https://github.com/xiaorouji/openwrt-passwall package/passwall
+git clone https://github.com/sbwml/luci-app-mosdns package/mosdns
 #wget https://github.com/vernesong/OpenClash/archive/master.zip  #下载OpenClash
 #unzip master.zip  #解压
 #cp -r OpenClash-master/luci-app-openclash package  #复制OpenClash软件包到OpenWrt
