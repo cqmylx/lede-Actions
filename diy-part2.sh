9b48fff251b0935ef1d57c10e9e4b53d9f695de8
cqmylx <67597378+cqmylx@users.noreply.github.com>
2023-10-22 21:46:41 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,19 +11,27 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
 sed -i 's/OpenWrt/LEDE/g' package/base-files/files/bin/config_generate
-sed -i 's/set luci.main.mediaurlbase/#set luci.main.mediaurlbase/g' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
-sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
+#sed -i 's/set luci.main.mediaurlbase/#set luci.main.mediaurlbase/g' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+
 
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
-rm -rf ./package/diy-ziyong/theme
-rm -rf ./package/diy-ziyong/luci-app-wrtbwmon-zh
-rm -rf ./package/diy-ziyong/wrtbwmon
-# rm -rf ./package/diy-ziyong/adguardhome
-rm -rf ./feeds/packages/net/adguardhome
-rm -rf ./feeds/packages/net/smartdns
-rm -rf ./feeds/packages/net/mosdns
-rm -rf ./feeds/luci/applications/luci-app-passwall
-rm -rf ./feeds/luci/applications/luci-app-ssr-plus
-rm -rf ./feeds/luci/applications/luci-app-openclash
-rm -rf ./feeds/luci/applications/luci-app-smartdns
-# rm -rf ./feeds/luci/applications/luci-app-adbyby-plus
+# 此脚本用处是：定制个性化固件
+
+
+# 更改默认主题
+sed -i 's/bootstrap/opentomcat/g' ./feeds/luci/collections/luci/Makefile
+
+# 指定内核版本
+sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=5.15/g' ./target/linux/x86/Makefile
+
+# 更改管理地址
+#sed -i 's/192.168.1.1/192.168.2.251/g' package/base-files/files/bin/config_generate
+
+# 设置密码为空
+sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/lean/default-settings/files/zzz-default-settings
+
+# 设置时间格式
+#sed -i 's/os.date()/os.date("%Y-%m-%d %H:%M:%S")/g' package/lean/autocore/files/*/index.htm
+
+# 只显示处理器信息
+#sed -i 's/${g}.*/${a}${b}${c}${d}${e}${f}${hydrid}/g' package/lean/autocore/files/x86/autocore
