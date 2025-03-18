6d706078b3001dcc6a1d15663282008fa1deda87
cqmylx <67597378+cqmylx@users.noreply.github.com>
2025-03-18 23:26:07 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,20 +1,43 @@
-# 此脚本用处是：添加第三方插件
-#git clone https://github.com/ujincn/smartdns.git ./package/lean/smartdns
-#git clone https://github.com/ujincn/luci-app-smartdns-compat.git ./package/lean/luci-app-smartdns
-#git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
 
-# 添加OpenClash插件
-echo "src-git openclash https://github.com/vernesong/openclash" >> "feeds.conf.default"
-
-# 添加ShadowSocksR Plus+插件
-echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
-
-# 添加PowerOff关机插件
-#git clone https://github.com/esirplayground/luci-app-poweroff.git package/luci-app-poweroff
-
-# 添加OpenTomcat主题
-#git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/luci-theme-opentomcat
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # 添加PassWall插件
-echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
-echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
+#echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
+#echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
+
+echo "src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main" >> "feeds.conf.default"
+
+# Add a feed source
+#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+#sed -i '$a src-git istore https://github.com/linkease/istore;main' feeds.conf.default
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+#git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+
+#git clone https://github.com/fw876/helloworld.git package/helloworld
+#git clone https://github.com/zijieKwok/istoreos-nas-packages.git package/istoreos-nas-packages
+#git clone https://github.com/animegasan/luci-app-quickstart package/luci-app-quickstart
+#git clone https://github.com/linkease/istore.git package/istoreos
+#git clone https://github.com/nikkinikki-org/OpenWrt-nikki.git;main package/openwrt-nikki
+rm -rf feeds/luci/applications/luci-app-passwall
+git clone https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall
+#git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall
+#git clone https://github.com/xiaorouji/openwrt-passwall2.git package/passwall2
+#rm -rf feeds/luci/applications/luci-app-smartdns
+#git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
+#rm -rf feeds/packages/net/v2ray-geodata
+#git clone https://github.com/godros/luci-app-godproxy.git ./package/lean/luci-app-godproxy
+#git clone https://github.com/garypang13/luci-app-dnsfilter.git ./package/lean/luci-app-dnsfilter
+#git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git ./package/lean/luci-app-jd-dailybonus
+#git clone https://github.com/iwrt/luci-app-ikoolproxy.git package/luci-app-ikoolproxy
