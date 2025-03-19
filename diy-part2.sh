394ba637e372d66cc11cc72b20a8ac75d789e51b
cqmylx <67597378+cqmylx@users.noreply.github.com>
2025-03-19 09:07:04 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,6 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
 sed -i 's/OpenWrt/LEDE/g' package/base-files/files/bin/config_generate
-#sed -i "s/set_opt main.mediaurlbase/#set_opt main.mediaurlbase/g" feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 #sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/emortal/default-settings/files/99-default-settings
 #sed -i "s/auto/'zh_cn'/g" feeds/luci/modules/luci-base/root/etc/config/luci
 #sed -i "/config internal languages/a\option zh_cn '简体中文 (Simplified Chinese)'\noption en  'English'" feeds/luci/modules/luci-base/root/etc/config/luci
@@ -23,8 +22,9 @@ sed -i 's/OpenWrt/LEDE/g' package/base-files/files/bin/config_generate
 
 # 更改默认主题
 #sed -i 's/bootstrap/opentomcat/g' ./feeds/luci/collections/luci/Makefile
-#sed -i 's/Bootstrap Theme (default)/Bootstrap Theme/g' ./feeds/luci/themes/luci-theme-bootstrap/Makefile
-#sed -i 's/LuCI modern OpenWrt theme/LuCI modern OpenWrt theme (default)/g' ./feeds/luci/themes/luci-theme-openwrt-2020/Makefile
+sed -i "s/set_opt main.mediaurlbase/#set_opt main.mediaurlbase/g" feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+sed -i 's/Bootstrap Theme (default)/Bootstrap Theme/g' ./feeds/luci/themes/luci-theme-bootstrap/Makefile
+sed -i 's/LuCI modern OpenWrt theme/LuCI modern OpenWrt theme (default)/g' ./feeds/luci/themes/luci-theme-openwrt-2020/Makefile
 
 # 指定内核版本
 #sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=5.15/g' ./target/linux/x86/Makefile
